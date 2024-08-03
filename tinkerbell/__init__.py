8cca65e6ecf55fd5800b32170715944ed4ce9dbe
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2024-08-03 17:55:00 -0400

    update

--- tinkerbell/__init__.py
+++ tinkerbell/__init__.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+SPDX-FileCopyrightText: 2024 Michael Czigler
+SPDX-License-Identifier: BSD-3-Clause
+
+Tinkerbell NeoPixel animation effect.
+"""
+
 from adafruit_led_animation.animation.comet import Comet
 from adafruit_led_animation.color import RED
 from adafruit_led_animation.group import AnimationGroup
@@ -11,6 +21,16 @@ from board import NEOPIXEL5
 from board import NEOPIXEL6
 from board import NEOPIXEL7
 from neopixel import NeoPixel
+
+FAIRY_LIGHT_N = const(100)
+
+
+def get_animation_group(*args):
+    return AnimationGroup(*args, sync=False)
+
+
+def get_animation_sequence(*members):
+    return AnimationSequence(*members)
 
 
 def get_pixels(pin):
@@ -34,9 +54,11 @@ def get_comet(pixels: NeoPixel):
 
 def main():
     pixels0 = get_pixels(NEOPIXEL0)
-    comet0 = get_comet(pixels0)
+    effect0 = get_comet(pixels0)
+    group0 = get_animation_group(effect0)
+    sequence = get_animation_sequence(group0)
     while True:
-        comet.animate()
+        sequence.animate()
 
 
 if __name__ == "__main__":
